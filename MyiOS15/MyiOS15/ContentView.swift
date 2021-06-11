198757e6486360be3b1dfd3719809b45ba9876f5
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-06-11 14:59:32 +0200

    Add New List

--- MyiOS15/MyiOS15/ContentView.swift
+++ MyiOS15/MyiOS15/ContentView.swift
@@ -7,10 +7,47 @@
 
 import SwiftUI
 
+struct Colors: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
 struct ContentView: View {
+    let imageUrl = "https://source.unsplash.com/random"
+    @State private var searchQuery: String = ""
+    @State private var colors: [Colors] = [Colors(name: "Blue"),Colors(name: "Red"),Colors(name: "Green"), Colors(name: "Yellow"), Colors(name: "White"), Colors(name: "Purple"), Colors(name: "Orange"), Colors(name: "Pink"), Colors(name: "Brown"), Colors(name: "Black"), Colors(name: "Gray"), Colors(name: "Clear")]
+
+    var searchResults: [Colors] {
+        if searchQuery.isEmpty {
+            return colors
+        } else {
+            return colors.filter({ $0.name.lowercased().contains(searchQuery.lowercased())})
+        }
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            List(searchResults) { color in
+                HStack {
+                    AsyncImage(url: URL(string: imageUrl)!, scale: 0.6, content: { image in
+                        image
+                    }, placeholder: {
+                        Image(systemName: "person.circle.fill")
+                            .renderingMode(.original)
+                            .font(.system(size: 60))
+                    })
+                    
+                        .frame(width: 60, height: 60)
+                        .clipShape(Circle())
+                    Text(color.name)
+                }
+            }
+            .searchable("Search color", text: $searchQuery, placement: .automatic){
+                Text("re").searchCompletion("red")
+                Text("b")
+            }
+            .navigationTitle("Async Images")
+        }
     }
 }
 
