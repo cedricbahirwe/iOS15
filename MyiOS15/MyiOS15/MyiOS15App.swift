ec311c55b3e3f92828a9532248668ff45b70bd2b
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-06-11 14:37:24 +0200

    Add Initial Project

---
+++ MyiOS15/MyiOS15/MyiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  MyiOS15App.swift
+//  MyiOS15
+//
+//  Created by Cédric Bahirwe on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
